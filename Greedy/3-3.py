f0d18a0bd792a802ecb72cf606b4b62827dfe025
윤예림 <95165319+YOONYERIM98@users.noreply.github.com>
2023-11-10 01:47:54 +0900

    Update 3-3.py

--- Greedy/3-3.py
+++ Greedy/3-3.py
@@ -1,12 +1,12 @@
 # 이것이 코딩 테스트다 3-3 숫자 카드 게임
-n, m = map(int, input().split())
+n, m = map(int, input().split()) # n * m 입력
 
 result = 0
 
-for _ in range(n):
+for _ in range(n): # n행 만큼 반복하면서 카드 값 입력
   data = list(map(int, input().split()))
-  minval = min(data)
+  minval = min(data) # n행의 가장 작은 카드 값 찾기
 
-  result = max(result, minval)
+  result = max(result, minval) # 가장 큰 카드 값 찾기
 
 print(result)
